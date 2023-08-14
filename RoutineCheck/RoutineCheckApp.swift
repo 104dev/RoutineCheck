7f7e02243cbd553f49c89169a3bf1ed2edbb1f32
104 <104developers@gmail.com>
2023-08-15 02:39:57 +0900

    Initial Commit

---
+++ RoutineCheck/RoutineCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineCheckApp.swift
+//  RoutineCheck
+//
+//  Created by jushiro watanabe on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct RoutineCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
