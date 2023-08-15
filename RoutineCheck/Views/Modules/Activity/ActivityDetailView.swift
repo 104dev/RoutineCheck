a7ac007669d236d3f0c6c9ee0472b686fa2f0542
104 <104developers@gmail.com>
2023-08-15 15:46:13 +0900

    アクティビティ詳細のフローティングメニューと削除機能を追加

--- RoutineCheck/Views/Modules/Activity/ActivityDetailView.swift
+++ RoutineCheck/Views/Modules/Activity/ActivityDetailView.swift
@@ -3,10 +3,12 @@ import SwiftUI
 struct ActivityDetailView: View {
     
     @EnvironmentObject var taskViewModel : TaskViewModel
-
     @EnvironmentObject var activityViewModel : ActivityViewModel
-
     let activity : Activity
+    @State private var activityFloetBtnSelected = false
+    @State private var isActivityEditModalPresented = false
+    @State var isActivityDeleteActionSheet = false
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         ZStack{
@@ -21,7 +23,7 @@ struct ActivityDetailView: View {
                     }
                     Spacer()
                 }.padding(.leading)
-                .padding(.top , 20)
+                    .padding(.top , 20)
                 List{
                     Section(header: Text("説明")){
                         if let activityDesc = activity.desc {
@@ -41,8 +43,72 @@ struct ActivityDetailView: View {
                     }
                 }
                 .background(Color(.systemGray6))
+            VStack{
+                Spacer()
+                if(activityFloetBtnSelected){
+                    VStack{
+                        HStack{
+                            Spacer()
+                            Button(action:{
+                                isActivityDeleteActionSheet.toggle()
+                            } ) {
+                                Spacer()
+                                ZStack{
+                                    Circle()
+                                        .foregroundColor(.blue)
+                                        .frame(width: 32, height: 32)
+                                    Image(systemName: "trash.fill")
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 12))
+                                }
+                                Text("アクティビティを削除")
+                                    .foregroundColor(.white)
+                                    .font(.caption)
+                                    .padding(4)
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color.blue)
+                                    .cornerRadius(3)
+                            }
+                            .frame(width: 240, height: 20)
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 16.0, trailing: 16.0))
+                            .actionSheet(isPresented: $isActivityDeleteActionSheet){
+                                ActionSheet(
+                                    title: Text("アクティビティの削除"),
+                                    message: Text("このアクティビティを削除してよろしいですか？"),
+                                    buttons: [
+                                        .destructive(Text("削除"), action: {
+                                            ActivityViewModel().deleteActivity(activity)
+                                            presentationMode.wrappedValue.dismiss()
+                                        }),
+                                        .cancel()
+                                    ]
+                                )
+                            }
+                        }
+                    }
+                }
+                
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        withAnimation(.easeInOut) {
+                            activityFloetBtnSelected.toggle()
+                        }
+                    }, label: {
+                        Image(systemName: activityFloetBtnSelected ? "xmark" : "pencil")
+                            .foregroundColor(.black)
+                            .font(.system(size: 24))
+                    })
+                    .frame(width: 60, height: 60)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(30.0)
+                    .shadow(color: .gray, radius: 3, x: 3, y: 3)
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 16.0, trailing: 16.0))
+                }
+            }
+            
         }
-
+        
     }
 }
 
